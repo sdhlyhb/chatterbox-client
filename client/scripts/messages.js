cef4cc8ca27a5c8c31fc041b2c474c0ce8fd56e1
Rob Scherer <robscherer6@gmail.com>
2022-03-28 23:33:01 -0400

    working through

--- client/scripts/messages.js
+++ client/scripts/messages.js
@@ -1,14 +1,18 @@
 // This object houses all the message _data_ for the app.
 // Treat it like a data structure - add methods to interact
 // with and manipulate the data.
+//_data: {0: msg1, 1: msg2....};
 
 var Messages = {
 
   // TODO: Define how you want to store your messages.
 
-  _data: undefined,
+  _data: {},
+  id: 0,
 
   // TODO: Define methods which allow you to retrieve from,
   // add to, and generally interact with the messages.
+  // addMessage: function (message) {
 
+  // }
 };
